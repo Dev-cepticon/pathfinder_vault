17b63914f4746635f18988681225a4f8b6620722
Dev-cepticon <clintenclark@gmail.com>
2025-02-04 16:40:51 -0500

    added background model -cfsm

---
+++ app/Models/Background.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Background extends Model
+{
+    /** @use HasFactory<\Database\Factories\BackgroundFactory> */
+    use HasFactory;
+
+    protected $guarded = [
+
+    ];
+
+    protected $casts = [
+        
+    ];
+}
